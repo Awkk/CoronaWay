6f7b58f228a526b79c856fecedd44a82a5ca74ca
Sehyung (Eric) Kim <kimeric97@hotmail.com>
2020-09-29 22:21:42 -0700

    layout -> activity_login.xml added signup instead textview layout -> activity_welcome base java\com\awisme\coronaway -> WelcomeActivity

---
+++ app/src/main/java/com/awisme/coronaway/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.awisme.coronaway;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class WelcomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
